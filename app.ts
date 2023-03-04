b200ab6ff26ddccf30f9354ba140b42683cb4d87
Pavel Ivshin <povelivshin@gmail.com>
2023-03-05 00:18:21 +0300

    task4

--- app.ts
+++ app.ts
@@ -1,76 +1,35 @@
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+
 interface IPost {
-  id: string;
-  title: string;
-  body: string;
-}
-interface IbyId {
-  [key: string]: IPost
-}
-interface INormalizeData {
-  byId: IbyId
-  allIds: string[]
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
 }
 
-const posts: IPost[] | [] = [
-  {
-    id: '62e69d5a5458aac0ed320b35',
-    title: 'id labore ex et quam laborum',
-    body: 'laudantium enim quasi est quidem magnam voluptate ipsam eostempora quo necessitatibusdolor quam autem quasireiciendis et nam sapiente accusantium'
-  },
-  {
-    id: '62e69d5a5458aac0ed320b1c',
-    title: 'quo vero reiciendis velit similique earum',
-    body: 'est natus enim nihil est dolore omnis voluptatem numquamet omnis occaecati quod ullam at voluptatem error expedita pariaturnihil sint nostrum voluptatem reiciendis et'
-  },
-  {
-    id: '62e69d5a5458aac0ed320b32',
-    title: 'odio adipisci rerum aut animi',
-    body: 'quia molestiae reprehenderit quasi aspernaturaut expedita occaecati aliquam eveniet laudantiumomnis quibusdam delectus saepe quia accusamus maiores nam estcum et ducimus et vero voluptates excepturi deleniti ratione'
-  },
-  {
-    id: '62e69d5a5458aac0ed320b39',
-    title: 'alias odio sit',
-    body: 'non et atqueoccaecati deserunt quas accusantium unde odit nobis qui voluptatemquia voluptas consequuntur itaque doloret qui rerum deleniti ut occaecati'
-  },
-  {
-    id: '62e69d5a5458aac0ed320b53',
-    title: 'vero eaque aliquid doloribus et culpa',
-    body: 'harum non quasi et rationetempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et'
-  },
-  {
-    id: '62e69d5a5458aac0ed320b19',
-    title: 'et fugit eligendi deleniti quidem qui sint nihil autem',
-    body: 'doloribus at sed quis culpa deserunt consectetur qui praesentiumaccusamus fugiat dictavoluptatem rerum ut voluptate autemvoluptatem repellendus aspernatur dolorem in'
-  },
-  {
-    id: '62e69d5a5458aac0ed320b25',
-    title: 'repellat consequatur praesentium vel minus molestias voluptatum',
-    body: 'maiores sed dolores similique labore et inventore etquasi temporibus esse sunt id eteos voluptatem aliquamratione corporis molestiae mollitia quia et magnam dolor'
+const getData = async (url: string): Promise<IPost[]> => {
+  let response;
+  try {
+    response = await fetch(url);
+  } catch {
+    throw new Error('error');
   }
-];
-  
-const normalizeData = (unnormalizedData: IPost[] | []): INormalizeData => {
-  const byId: IbyId = {};
-  const allIds: string[] = [];
-  [...unnormalizedData].forEach((el: IPost) => {
-    byId[el.id] = {...el};
-    allIds.push(el.id);
-  });
+  const body: IPost[] = await response.json();
+  return body;
+}
 
-  return {
-    byId,
-    allIds,
-  }
-};
+getData(COMMENTS_URL)
+  .then(data => {
+    data.forEach((el) => {
+      console.log(`ID ${el.id}, Email: ${el.email}`);
+    })
+  }).catch((err: Error) => console.log(err.message));
 
-console.log(normalizeData(posts));
 /**
- * {
- *    byId: {
- *      62e69d5a5458aac0ed320b35: { id: '...', title: '...', body: '...' },
- *      62e69d5a5458aac0ed320b1c: { id: '...', title: '...', body: '...' },
- *      ...
- *    },
- *    allIds: ['62e69d5a5458aac0ed320b35', '62e69d5a5458aac0ed320b1c', ...]
- * }
+ * ID: 1, Email: Eliseo...
+ * ID: 2, Email: Jayne_Kuhic...
+ * ID: 3, Email: Nikita...
+ * ID: 4, Email: Lew...
+ * ...
  */
